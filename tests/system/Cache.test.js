57946e37e43fbbec3d7c195cec0f62e8086b09cc
Trevor <tcrupi@purdue.edu>
2019-05-17 20:47:52 -0400

    Added simple template engine

--- tests/system/Cache.test.js
+++ tests/system/Cache.test.js
@@ -13,12 +13,12 @@ test('setup actually creates the folder', () => {
 });
 
 test('cache returns correct object when creating a file', () => {
-  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.html');
+  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.handlebars');
   expect(system.Cache('Test').cache([__dirname, 'TestFolder', 'test2.md'])).toEqual({ created: true, path: fullPath, contents: '<h1 id=\"test\">test</h1>\n' });
 });
 
 test('cache returns bad object when can\'t find a file', () => {
-  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test4.md.html');
+  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test4.md.handlebars');
   expect(system.Cache('Test').cache([__dirname, 'TestFolder', 'test4.md']).created).toEqual(false);
 });
 
@@ -28,22 +28,22 @@ test('cache actually creates an HTML file', () => {
 });
 
 test('retrieveFullCachePath gives correct full path to cache file', () => {
-  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.html');
+  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.handlebars');
   expect(system.Cache('Test').retrieveFullCachePath('test2.md')).toBe(fullPath);
 });
 
 test('remove correctly returns when given good file', () => {
-  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.html');
-  expect(system.Cache('Test').remove('test2.md.html')).toEqual({ removed: true, path: fullPath });
+  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.handlebars');
+  expect(system.Cache('Test').remove('test2.md.handlebars')).toEqual({ removed: true, path: fullPath });
 });
 
 test('remove correctly returns false when given bad file', () => {
-  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test3.md.html');
-  expect(system.Cache('Test').remove('test3.md.html').removed).toBe(false);
+  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test3.md.handlebars');
+  expect(system.Cache('Test').remove('test3.md.handlebars').removed).toBe(false);
 });
 
 test('remove correctly removes file from cache', () => {
-  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.html');
+  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.handlebars');
   expect(fs.existsSync(fullPath)).toBe(false);
 });
 
@@ -65,6 +65,6 @@ test('delete correctly returns given bad folder', () => {
 });
 
 test('delete correctly deletes project folder from cache', () => {
-  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.html');
+  const fullPath = path.join(process.cwd(), 'lib', 'system', 'cache', 'Test', 'test2.md.handlebars');
   expect(fs.existsSync(fullPath)).toBe(false);
 });
